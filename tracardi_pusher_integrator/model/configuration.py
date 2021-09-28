12b17eb094dc88e1d849000415ab3bd49cd5c9aa
emge1 <88485481+emge1@users.noreply.github.com>
2021-09-28 17:49:47 +0200

    Update configuration.py

--- tracardi_pusher_integrator/model/configuration.py
+++ tracardi_pusher_integrator/model/configuration.py
@@ -6,13 +6,22 @@ class PusherClient(BaseModel):
     instance_id: str
     secret_key: str
 
-class PusherIntegratorConfiguration(BaseModel):
+        
+class PusherPlatform(BaseModel):
+    web: bool
+    ios: bool
+    android: bool
+
+
+class PusherConfiguration(BaseModel):
     interests: Optional[list]
     user_ids: Optional[list]
+
+    title: str
+    body: str
+    data: Optional[dict]
+
     time_to_live: Optional[int]
-    title: Optional[str]
-    body: Optional[str]
     icon: Optional[str]
     deep_link: Optional[str]
     hide_notification_if_site_has_focus: Optional[bool]
-    data: Optional[dict]
