aa8d17851cbb5382e793b4d1fa9c8cc15e752d6b
emge1 <88485481+emge1@users.noreply.github.com>
2021-09-28 17:51:00 +0200

    Update test_plugin.py

--- tracardi_pusher_integrator/test manual/test/test_plugin.py
+++ tracardi_pusher_integrator/test manual/test/test_plugin.py
@@ -4,40 +4,21 @@ from tracardi_pusher_integrator.plugin import PusherIntegratorAction
 
 
 async def main():
-    init = {"instance_id": None,
-            "secret_key": None,
+    init = {"instance_id": "3d2e5ae8-bff1-40e9-96c1-beb37384ebbc",
+            "secret_key": "D54335B8E565D11DA91E9911AD412A0667DBC3EDBE974266AE926BE50C840D54",
             "interests": None,
-            "user_ids": None,
-            "publish_body": {
-                'apns': {
-                    'aps': {
-                        'alert': {
-                            'title': None,
-                            'body': None,
-                        },
-                        'data': {},
-                    },
-                },
-                'fcm': {
-                    'notification': {
-                        'title': None,
-                        'body': None,
-                    },
-                    'data': {},
-                },
-                'web': {
-                    'time_to_live': None,
-                    'notification': {
-                        'title': None,
-                        'body': None,
-                        'icon': None,
-                        'deep_link': None,
-                        'hide_notification_if_site_has_focus': False,
-                    },
-                    'data': {},
-                },
-            },
-            }
+            "user_ids": ["hgCkry9t9r0mtHE6aKqUOT6_FNpcl3l1TNSom-og_M4"],
+            "web": True,
+            "ios": False,
+            "android": False,
+            "title": "Hello",
+            "body": "Hello world",
+            "data": {"some": "data"},
+            "time_to_live": 111111,
+            "icon": None,
+            "deep_link": None,
+            "hide_notification_if_site_has_focus": False}
+
 
     plugin = PusherIntegratorAction(**init)
 
