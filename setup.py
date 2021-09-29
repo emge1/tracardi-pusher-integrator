70dc94695db50881d22a2f3808064f014a88f6ec
emge1 <88485481+emge1@users.noreply.github.com>
2021-09-29 12:06:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tracardi-pusher-integrator',
-    version='0.1.0',
+    version='0.1',
     description='The purpose of this plugin is to connect Tracardi with Pusher Beams.',
     long_description=long_description,
     long_description_content_type="text/markdown",
